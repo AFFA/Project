128c0b150564b54b9f39e0004a6fc9a2f4f5acc6
ttalpsep <ttalpsep@itcollege.ee>
2012-11-10 02:48:03 +0200

    YCharts klasside väike parandus

--- AFFA/Scraperid/WebClientEx.cs
+++ AFFA/Scraperid/WebClientEx.cs
@@ -13,6 +13,11 @@ namespace AFFA.Scraperid
 
         protected override WebRequest GetWebRequest(Uri address)
         {
+            this.Headers.Add("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.0; et; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16 ( .NET CLR 3.5.30729; .NET4.0C)");
+            this.Headers.Add("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            this.Headers.Add("Accept-Language: et,et-ee;q=0.5");
+            this.Headers.Add("Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+            this.Headers.Add("Keep-Alive: 115");
             WebRequest request = base.GetWebRequest(address);
             if (request is HttpWebRequest)
             {
--- AFFA/Scraperid/YChartsScraper.cs
+++ AFFA/Scraperid/YChartsScraper.cs
@@ -15,11 +15,6 @@ namespace AFFA.Scraperid
         {
             using (var client = new WebClientEx())
             {
-                client.Headers.Add("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.0; et; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16 ( .NET CLR 3.5.30729; .NET4.0C)");
-                client.Headers.Add("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-                client.Headers.Add("Accept-Language: et,et-ee;q=0.5");
-                client.Headers.Add("Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7");
-                client.Headers.Add("Keep-Alive: 115");
 
                 var response1 = client.DownloadString("https://ycharts.com/login?next=");
                 Regex rSector = new Regex("csrfmiddlewaretoken' value='(.*?)'");
@@ -37,11 +32,6 @@ namespace AFFA.Scraperid
                                    {"auth_submit", "Sign In"},
                                    
                                };
-                    client.Headers.Add("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.0; et; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16 ( .NET CLR 3.5.30729; .NET4.0C)");
-                    client.Headers.Add("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-                    client.Headers.Add("Accept-Language: et,et-ee;q=0.5");
-                    client.Headers.Add("Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7");
-                    client.Headers.Add("Keep-Alive: 115");
                     client.Headers.Add("Referer: https://ycharts.com/login?next=");
                     var response2 = client.UploadValues("https://ycharts.com/login", data);
                     //MessageBox.Show(Encoding.Default.GetString(response2));
